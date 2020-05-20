31213eb9d7b0908b01adade5948d760483d778d6
Amanda Ricketts <avr@uoregon.edu>
2020-05-19 17:16:24 -0700

    data replacement

--- analysis/organize_data.R
+++ analysis/organize_data.R
@@ -9,10 +9,10 @@
 #source in any useful functions
 source("check_packages.R")
 source("useful_functions.R")
-load("input/usa_00007.dat.gz")
+load("input/usa_00008.dat.gz")
 
 #read in the IPUMS fixed-width data from gzip file and ensure all variables are read in as integers
-ipumsdata <- read_fwf("input/usa_00007.dat.gz", 
+ipumsdata <- read_fwf("input/usa_00008.dat.gz", 
                       col_positions = fwf_positions(start=c(1,5, 11,19,32,42,55,67,68,72,82,83,86,87,90,91,92,93),
                                                     end  =c(4,10,18,31,41,54,66,67,71,81,82,85,86,89,90,91,92,94),
                                                     col_names=c("year","sample","serial","cbserial","hhwt",
